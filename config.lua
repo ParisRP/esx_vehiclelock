4b76e13b13501132cb3a1642a0d1be063bb3e27f
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-03 22:09:34 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Locale = 'fr' -- Langue du script
+Config.KeyControl = 0x2D -- Touche Insert pour ouvrir le menu des clés
+Config.LockControl = 82  -- Touche "U" pour ouvrir/fermer le véhicule
+
+-- Configuration pour l'intervalle de vérification
+Config.CheckInterval = 1000
