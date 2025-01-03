deeb9774b3850eaa03dbd3f9d4680f8bc5740a35
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-03 22:00:46 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Paris RP'
+description 'Système de gestion des clés de véhicules avec ESX et esx_vehicleshop'
+version '1.0.0'
+
+dependency 'es_extended'
+dependency 'mysql-async'
+dependency 'esx_vehicleshop' -- Dépendance pour les véhicules du shop
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'server.lua',
+    'config.lua'
+}
+
+files {
+    'sql/schema.sql'
+}
+
+locales {
+    'locales/fr.lua'
+}
