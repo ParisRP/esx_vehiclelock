dc55d996456cdf733f78f5c2617903afddac56ee
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-03 22:03:49 +0100

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,8 @@
+Locales['fr'] = {
+    ['vehicle_locked'] = 'Le véhicule est verrouillé.',
+    ['vehicle_unlocked'] = 'Le véhicule est déverrouillé.',
+    ['no_key'] = 'Vous n\'avez pas la clé pour ce véhicule.',
+    ['key_added'] = 'Vous avez une clé pour ce véhicule.',
+    ['key_removed'] = 'Clé du véhicule supprimée.',
+    ['vehicle_owner'] = 'Vous êtes maintenant le propriétaire du véhicule.',
+}
